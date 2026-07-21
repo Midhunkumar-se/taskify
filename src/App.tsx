540411a0b01afb59fd17d053f5af900c1e14cbf0
Midhunkumar-se <midhu025@gmail.com>
2026-07-21 16:57:11 +0530

    basic type, object type, array of object type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,24 @@ import { useState } from "react";
 
 import "./App.css";
 
+let name = "Midhun";
+let age = 5;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+type Person = {
+  name: string;
+  age?: number;
+};
+
+let person: Person = {
+  name: "Midhun",
+  // age: 33
+};
+
+let person2: Person[] = [];
+
 function App() {
   const [count, setCount] = useState(0);
 
